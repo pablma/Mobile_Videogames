f3e6d04c4456e9aa83cb1d9fbcd5dd236f52c967
rebo95 <arebored@ucm.es>
2019-11-16 15:06:19 +0100

    MV Assignment1 : All assets added

--- Mobiles/Logic/src/main/java/com/example/logic/Assets.java
+++ Mobiles/Logic/src/main/java/com/example/logic/Assets.java
@@ -5,13 +5,104 @@ import com.example.engine.Rect;
 
 public class Assets { // debería ir en logic
 
+    //Player
+    public static Image _blackPlayerImg;
+    public static Rect _blackPlayerRect;
+
+    public static Image _whitePlayerImg;
+    public static Rect _withePlayerRect;
+
+
+    //Balls
     public static Image _blackBallImg;
     public static Rect _blackBallRect;
 
+    public static Image _witeBallImg;
+    public static Rect _whiteBallRect;
+
+
+    //Background Arrows
+    public static Image _backgroundArrowsImg;
+    public static Rect _backgroundArrowsRect;
+
+
+    //Background Color Images
     public static Image _greenBackgroundImg;
     public static Rect _greenBackgroundRect;
 
-    public static Image _backgroundArrowsImg;
-    public static Rect _backgroundArrowsRect;
+    public static Image _greenBlueBackgroundImg;
+    public static Rect _greenBlueBackgroundRect;
+
+    public static Image _softBlueBackgroundImg;
+    public static Rect _softBlueBackgroundRect;
+
+    public static Image _blueBackgroundImg;
+    public static Rect _blueBackgroundRect;
+
+    public static Image _purpleBackgroundImg;
+    public static Rect _purpleBackgroundRect;
+
+    public static Image _darkBlueBackgroundImg;
+    public static Rect _darkBlueBackgroundRect;
+
+    public static Image _orengeBackgroundImg;
+    public static Rect _orangeBackgroundRect;
+
+    public static Image _redBackgroundImg;
+    public static Rect _redBackgroundRect;
+
+    public static Image _brownBackgroundImg;
+    public static Rect _brownBackgroundRect;
+
+
+    //Interface
+    public static Image _gameOverImg;
+    public static Rect _gameOverRect;
+
+    public static Image _howToPlayImg;
+    public static Rect _howToPlayRect;
+
+    public static Image _instructionsImg;
+    public static Rect _instructionsRect;
+
+    public static Image _playAgainImg;
+    public static Rect _playAgainRect;
+
+    public static Image _tapToPlayImg;
+    public static Rect _tapToPlayRect;
+
+    public static Image _switchDashLogoImg;
+    public static Rect _switchDashLogoRect;
+
+    //Numbers
+    public static Image _zeroImg;
+    public static Rect _zeroRect;
+
+    public static Image _oneImg;
+    public static Rect _oneRect;
+
+    public static Image _twoImg;
+    public static Rect _twoRect;
+
+    public static Image _threeImg;
+    public static Rect _threeRect;
+
+    public static Image _fourImg;
+    public static Rect _fourRect;
+
+    public static Image _fiveImg;
+    public static Rect _fiveRect;
+
+    public static Image _sixImg;
+    public static Rect _sixRect;
+
+    public static Image _sevenImg;
+    public static Rect _sevenRect;
+
+    public static Image _eightImg;
+    public static Rect _eightRect;
+
+    public static Image _nineImg;
+    public static Rect _nineRect;
 
 }
