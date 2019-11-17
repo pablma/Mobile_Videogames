26432facad78b78806e6cbf41034b78694b4000c
rebo95 <arebored@ucm.es>
2019-11-17 01:21:48 +0100

    MV Assignment1 : AssetsScript Updated, Loading State implementation

--- Mobiles/Logic/src/main/java/com/example/logic/Assets.java
+++ Mobiles/Logic/src/main/java/com/example/logic/Assets.java
@@ -2,107 +2,147 @@ package com.example.logic;
 
 import com.example.engine.Image;
 import com.example.engine.Rect;
+import com.example.engine.Sprite;
 
 public class Assets { // debería ir en logic
 
     //Player
+    public static Sprite _blackPlayerSprite;
     public static Image _blackPlayerImg;
     public static Rect _blackPlayerRect;
 
+    public static Sprite _whitePlayerSprite;
     public static Image _whitePlayerImg;
     public static Rect _withePlayerRect;
 
 
     //Balls
+    public static Sprite _blackBallSprite;
     public static Image _blackBallImg;
     public static Rect _blackBallRect;
 
-    public static Image _witeBallImg;
+    public static Sprite _whiteBallSprite;
+    public static Image _whiteBallImg;
     public static Rect _whiteBallRect;
 
 
     //Background Arrows
+    public static Sprite _backgroundArrowsSprite;
     public static Image _backgroundArrowsImg;
     public static Rect _backgroundArrowsRect;
 
 
     //Background Color Images
+    public static Sprite _greenBackgroundSprite;
     public static Image _greenBackgroundImg;
     public static Rect _greenBackgroundRect;
 
+    public static Sprite _greenBlueBackgroundSprite;
     public static Image _greenBlueBackgroundImg;
     public static Rect _greenBlueBackgroundRect;
 
+    public static Sprite _softBlueBackgroundSprite;
     public static Image _softBlueBackgroundImg;
     public static Rect _softBlueBackgroundRect;
 
+    public static Sprite _blueBackgroundSprite;
     public static Image _blueBackgroundImg;
     public static Rect _blueBackgroundRect;
 
+    public static Sprite _purpleBackgroundSprite;
     public static Image _purpleBackgroundImg;
     public static Rect _purpleBackgroundRect;
 
+    public static Sprite _darkBlueBackgroundSprite;
     public static Image _darkBlueBackgroundImg;
     public static Rect _darkBlueBackgroundRect;
 
+    public static Sprite _orengeBackgroundSprite;
     public static Image _orengeBackgroundImg;
     public static Rect _orangeBackgroundRect;
 
+    public static Sprite _redBackgroundImgSprite;
     public static Image _redBackgroundImg;
     public static Rect _redBackgroundRect;
 
+    public static Sprite _brownBackgroundSprite;
     public static Image _brownBackgroundImg;
     public static Rect _brownBackgroundRect;
 
 
     //Interface
+    public static Sprite _gameOverSprite;
     public static Image _gameOverImg;
     public static Rect _gameOverRect;
 
+    public static Sprite _howToPlaySprite;
     public static Image _howToPlayImg;
     public static Rect _howToPlayRect;
 
+    public static Sprite _instructionsSprite;
     public static Image _instructionsImg;
     public static Rect _instructionsRect;
 
+    public static Sprite _playAgainISprite;
     public static Image _playAgainImg;
     public static Rect _playAgainRect;
 
+    public static Sprite _tapToPlaySprite;
     public static Image _tapToPlayImg;
     public static Rect _tapToPlayRect;
 
+    public static Sprite _switchDashLogoSprite;
     public static Image _switchDashLogoImg;
     public static Rect _switchDashLogoRect;
 
     //Numbers
+    public static Sprite _zeroSprite;
     public static Image _zeroImg;
     public static Rect _zeroRect;
 
+    public static Sprite _oneSprite;
     public static Image _oneImg;
     public static Rect _oneRect;
 
+    public static Sprite _twoSprite;
     public static Image _twoImg;
     public static Rect _twoRect;
 
+    public static Sprite _threeSprite;
     public static Image _threeImg;
     public static Rect _threeRect;
 
+    public static Sprite _fourSprite;
     public static Image _fourImg;
     public static Rect _fourRect;
 
+    public static Sprite _fiveSprite;
     public static Image _fiveImg;
     public static Rect _fiveRect;
 
+    public static Sprite _sixSprite;
     public static Image _sixImg;
     public static Rect _sixRect;
 
+    public static Sprite _blackSprite;
     public static Image _sevenImg;
     public static Rect _sevenRect;
 
+    public static Sprite _eigthSprite;
     public static Image _eightImg;
     public static Rect _eightRect;
 
+    public static Sprite _nineSprite;
     public static Image _nineImg;
     public static Rect _nineRect;
+
+    public static  Sprite _crosSprite;
+    public static Image _crosImg;
+    public static Rect _crosRect;
+
+    public static  Sprite _questionSprite;
+    public static Image _questionImg;
+    public static Rect _questionRect;
+
 
 }
---
+++ Mobiles/Logic/src/main/java/com/example/logic/LoadingState.java
@@ -0,0 +1,223 @@
+package com.example.logic;
+
+import com.example.engine.Game;
+import com.example.engine.Graphics;
+import com.example.engine.Image;
+import com.example.engine.Rect;
+import com.example.engine.Sprite;
+import com.example.engine.State;
+
+public class LoadingState extends State {
+
+    public LoadingState(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        Graphics _graphics = _game.getGraphics();
+
+        //Player
+        Assets._blackPlayerImg  = _graphics.newImage("players.png");
+        Assets._blackPlayerRect  = new Rect(0,  Assets._blackPlayerImg.getHeight() / 2,
+                Assets._blackPlayerImg.getWidth(),  Assets._blackPlayerImg.getHeight() / 2);
+        Assets._blackPlayerSprite = new Sprite(_graphics, Assets._blackPlayerImg, Assets._blackPlayerRect);
+
+        Assets._whitePlayerImg = Assets._blackPlayerImg;
+        Assets._withePlayerRect = new Rect(0,  0,
+                Assets._whitePlayerImg.getWidth(),  Assets._whitePlayerImg.getHeight() / 2);
+        Assets._whitePlayerSprite = new Sprite(_graphics, Assets._blackPlayerImg, Assets._withePlayerRect);
+
+
+        //Balls
+        Assets._blackBallImg = _graphics.newImage("balls.png");
+        Assets._blackBallRect = new Rect(0,  Assets._blackBallImg.getHeight() / 2,
+                Assets._blackBallImg.getWidth() / 10,  Assets._blackBallImg.getHeight() / 2);
+        Assets._blackBallSprite = new Sprite(_graphics, Assets._blackBallImg, Assets._blackBallRect);
+
+
+        Assets._whiteBallImg = Assets._blackBallImg;
+        Assets._whiteBallRect  = new Rect(0,  0,
+                Assets._whiteBallImg.getWidth() / 10,  Assets._whiteBallImg.getHeight() / 2);
+        Assets._whiteBallSprite  = new Sprite(_graphics, Assets._whiteBallImg, Assets._whiteBallRect);
+
+
+        //Background Arrows
+        Assets._backgroundArrowsImg  = _graphics.newImage("arrowsBackground.png");
+        Assets._backgroundArrowsRect  = new Rect(0,0, Assets._backgroundArrowsImg.getWidth(), Assets._backgroundArrowsImg.getHeight());
+        Assets._backgroundArrowsSprite  = new Sprite(_graphics, Assets._backgroundArrowsImg, Assets._backgroundArrowsRect);
+
+
+        //Background Color Images
+        Assets._greenBackgroundImg = _graphics.newImage("backgrounds.png");
+        Assets._greenBackgroundRect  = new Rect(0,0,
+                Assets._greenBackgroundImg.getWidth() / 9, Assets._greenBackgroundImg.getHeight());
+        Assets._greenBackgroundSprite = new Sprite(_graphics, Assets._greenBackgroundImg, Assets._greenBackgroundRect);
+
+        Assets._greenBlueBackgroundImg  = Assets._greenBackgroundImg;
+        Assets._greenBlueBackgroundRect  = new Rect((Assets._greenBlueBackgroundImg.getWidth() / 9),0,
+                Assets._greenBlueBackgroundImg.getWidth() / 9, Assets._greenBlueBackgroundImg.getHeight());
+        Assets._greenBlueBackgroundSprite = new Sprite(_graphics, Assets._greenBlueBackgroundImg, Assets._greenBlueBackgroundRect);
+
+        Assets._softBlueBackgroundImg  = Assets._greenBackgroundImg;
+        Assets._softBlueBackgroundRect  = new Rect(2 * (Assets._greenBlueBackgroundImg.getWidth() / 9),0,
+                 Assets._softBlueBackgroundImg.getWidth() / 9, Assets._softBlueBackgroundImg.getHeight());
+        Assets._softBlueBackgroundSprite = new Sprite(_graphics, Assets._softBlueBackgroundImg, Assets._softBlueBackgroundRect);
+
+
+        Assets._blueBackgroundImg  = Assets._greenBackgroundImg;
+        Assets._blueBackgroundRect  = new Rect(3 * (Assets._greenBlueBackgroundImg.getWidth() / 9),0,
+                Assets._blueBackgroundImg.getWidth() / 9, Assets._blueBackgroundImg.getHeight());
+        Assets._blueBackgroundSprite = new Sprite(_graphics, Assets._blueBackgroundImg, Assets._blueBackgroundRect);
+
+
+        Assets._purpleBackgroundImg = Assets._greenBackgroundImg;
+        Assets._purpleBackgroundRect  = new Rect(4 * (Assets._greenBlueBackgroundImg.getWidth() / 9),0,
+                Assets._purpleBackgroundImg.getWidth() / 9, Assets._purpleBackgroundImg.getHeight());
+        Assets._purpleBackgroundSprite = new Sprite(_graphics, Assets._purpleBackgroundImg, Assets._purpleBackgroundRect);
+
+
+        Assets._darkBlueBackgroundImg  = Assets._greenBackgroundImg;
+        Assets._darkBlueBackgroundRect  = new Rect(5 * (Assets._greenBlueBackgroundImg.getWidth() / 9),0,
+                Assets._darkBlueBackgroundRect.getWidth() / 9, Assets._darkBlueBackgroundRect.getHeight());
+        Assets._darkBlueBackgroundSprite = new Sprite(_graphics, Assets._darkBlueBackgroundImg, Assets._darkBlueBackgroundRect);
+
+
+        Assets._orengeBackgroundImg  = Assets._greenBackgroundImg;
+        Assets._orangeBackgroundRect  = new Rect(6 * (Assets._greenBlueBackgroundImg.getWidth() / 9),0,
+                Assets._orengeBackgroundImg.getWidth() / 9, Assets._orengeBackgroundImg.getHeight());
+        Assets._orengeBackgroundSprite = new Sprite(_graphics, Assets._orengeBackgroundImg, Assets._orangeBackgroundRect);
+
+
+        Assets._redBackgroundImg  =Assets._greenBackgroundImg;
+        Assets._redBackgroundRect  = new Rect(7 * (Assets._greenBlueBackgroundImg.getWidth() / 9),0,
+                Assets._redBackgroundImg.getWidth() / 9, Assets._redBackgroundImg.getHeight());
+        Assets._redBackgroundImgSprite = new Sprite(_graphics, Assets._redBackgroundImg, Assets._redBackgroundRect);
+
+        Assets._brownBackgroundImg  = Assets._greenBackgroundImg;
+        Assets._brownBackgroundRect  = new Rect(8 * (Assets._greenBlueBackgroundImg.getWidth() / 9),0,
+                Assets._brownBackgroundImg.getWidth() / 9, Assets._brownBackgroundImg.getHeight());
+        Assets._brownBackgroundSprite = new Sprite(_graphics, Assets._brownBackgroundImg, Assets._brownBackgroundRect);
+
+
+        //Interface
+        Assets._gameOverImg = _graphics.newImage("gameOver.png");
+        Assets._gameOverRect  = new Rect(0,0,
+                Assets._gameOverImg.getWidth(), Assets._gameOverImg.getHeight());
+        Assets._gameOverSprite = new Sprite(_graphics, Assets._gameOverImg, Assets._gameOverRect);
+
+
+        Assets._howToPlayImg = _graphics.newImage("howToPlay.png");
+        Assets._howToPlayRect  = new Rect(0,0,
+                Assets._howToPlayImg.getWidth(), Assets._howToPlayImg.getHeight());
+        Assets._howToPlaySprite = new Sprite(_graphics, Assets._howToPlayImg, Assets._howToPlayRect);
+
+
+        Assets._instructionsImg = _graphics.newImage("instructions.png");
+        Assets._instructionsRect  = new Rect(0,0,
+                Assets._instructionsImg.getWidth(), Assets._instructionsImg.getHeight());
+        Assets._instructionsSprite = new Sprite(_graphics, Assets._instructionsImg, Assets._instructionsRect);
+
+
+        Assets._playAgainImg = _graphics.newImage("playAgain.png");
+        Assets._playAgainRect  = new Rect(0,0,
+                Assets._playAgainImg.getWidth(), Assets._playAgainImg.getHeight());
+        Assets._playAgainISprite = new Sprite(_graphics, Assets._playAgainImg, Assets._playAgainRect);
+
+
+        Assets._tapToPlayImg = _graphics.newImage("tapToPlay.png");
+        Assets._tapToPlayRect  = new Rect(0,0,
+                Assets._tapToPlayImg.getWidth(), Assets._tapToPlayImg.getHeight());
+        Assets._tapToPlaySprite = new Sprite(_graphics, Assets._tapToPlayImg, Assets._tapToPlayRect);
+
+
+        Assets._switchDashLogoImg = _graphics.newImage("switchDashLogo.png");
+        Assets._switchDashLogoRect  = new Rect(0,0,
+                Assets._switchDashLogoImg.getWidth(), Assets._switchDashLogoImg.getHeight());
+        Assets._switchDashLogoSprite = new Sprite(_graphics, Assets._switchDashLogoImg, Assets._switchDashLogoRect);
+
+
+        //Numbers
+        Assets._zeroImg  = _graphics.newImage("scoreFont.png");
+        Assets._zeroRect  = new Rect(7 * (Assets._nineImg.getWidth() / 15),3 * (Assets._zeroImg.getHeight() / 7),
+                Assets._zeroImg.getWidth() / 15, Assets._zeroImg.getHeight() / 7);
+        Assets._zeroSprite = new Sprite(_graphics, Assets._zeroImg, Assets._zeroRect);
+
+        Assets._oneImg = Assets._zeroImg;
+        Assets._oneRect  = new Rect(8 * (Assets._nineImg.getWidth() / 15),3 * (Assets._zeroImg.getHeight() / 7),
+                Assets._oneImg.getWidth() / 15, Assets._oneImg.getHeight() / 7);
+        Assets._oneSprite = new Sprite(_graphics, Assets._oneImg, Assets._oneRect);
+
+        Assets._twoImg = Assets._zeroImg;
+        Assets._twoRect  = new Rect(9 * (Assets._nineImg.getWidth() / 15),3 * (Assets._zeroImg.getHeight() / 7),
+                Assets._twoImg.getWidth() / 15, Assets._twoImg.getHeight() / 7);
+        Assets._twoSprite = new Sprite(_graphics, Assets._twoImg, Assets._twoRect);
+
+        Assets._threeImg = Assets._zeroImg;
+        Assets._threeRect  = new Rect(10 * (Assets._nineImg.getWidth() / 15),3 * (Assets._zeroImg.getHeight() / 7),
+                Assets._threeImg.getWidth() / 15, Assets._threeImg.getHeight() / 7);
+        Assets._threeSprite = new Sprite(_graphics, Assets._threeImg, Assets._threeRect);
+
+        Assets._fourImg = Assets._zeroImg;
+        Assets._fourRect  = new Rect(11 * (Assets._nineImg.getWidth() / 15),3 * (Assets._zeroImg.getHeight() / 7),
+                Assets._fourImg.getWidth() / 15, Assets._fourImg.getHeight() / 7);
+        Assets._fourSprite = new Sprite(_graphics, Assets._fourImg, Assets._fourRect);
+
+        Assets._fiveImg = Assets._zeroImg;
+        Assets._fiveRect  = new Rect(12 * (Assets._nineImg.getWidth() / 15),3 * (Assets._zeroImg.getHeight() / 7),
+                Assets._fiveImg.getWidth() / 15, Assets._fiveImg.getHeight() / 7);
+        Assets._fiveSprite = new Sprite(_graphics, Assets._fiveImg, Assets._fiveRect);
+
+        Assets._sixImg = Assets._zeroImg;
+        Assets._sixRect  = new Rect(13 * (Assets._nineImg.getWidth() / 15),3 * (Assets._zeroImg.getHeight() / 7),
+                Assets._sixImg.getWidth() / 15, Assets._sixImg.getHeight() / 7);
+        Assets._sixSprite = new Sprite(_graphics, Assets._sixImg, Assets._sixRect);
+
+        Assets._sevenImg = Assets._zeroImg;
+        Assets._sevenRect  = new Rect(14 * (Assets._nineImg.getWidth() / 15),3 * (Assets._zeroImg.getHeight() / 7),
+                Assets._sevenImg.getWidth() / 15, Assets._sevenImg.getHeight());
+        Assets._blackSprite = new Sprite(_graphics, Assets._sevenImg, Assets._sevenRect);
+
+        Assets._eightImg = Assets._zeroImg;
+        Assets._eightRect  = new Rect(0,4 * (Assets._zeroImg.getHeight() / 7),
+                Assets._eightImg.getWidth() / 15, Assets._eightImg.getHeight() / 7);
+        Assets._eigthSprite  = new Sprite(_graphics, Assets._eightImg, Assets._eightRect);
+
+        Assets._nineImg = Assets._zeroImg;
+        Assets._nineRect  = new Rect((Assets._nineImg.getWidth() / 15),4 * (Assets._zeroImg.getHeight() / 7),
+                Assets._nineImg.getWidth() / 15, Assets._nineImg.getHeight() / 7);
+        Assets._nineSprite = new Sprite(_graphics, Assets._nineImg, Assets._nineRect);
+
+
+        //Buttons
+        Assets._crosImg = _graphics.newImage("buttons.png");
+        Assets._crosRect   = new Rect(Assets._crosImg.getWidth() / 10,0,
+                Assets._crosImg.getWidth() / 10, Assets._crosImg.getHeight());
+        Assets._crosSprite = new Sprite(_graphics, Assets._crosImg, Assets._crosRect);
+
+        Assets._questionImg = Assets._crosImg;
+        Assets._questionRect  = new Rect(0,0,
+                Assets._questionImg.getWidth() / 10, Assets._questionImg.getHeight());
+        Assets._questionSprite = new Sprite(_graphics, Assets._questionImg, Assets._questionRect);
+    }
+
+    @Override
+    public void present(float deltaTime) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
