fa0c7bb15a1d0506e355bee3772e7c5e6c76fa1f
pablma <pablma11@ucm.es>
2019-11-23 10:12:17 +0100

    GVM A1: AndroidGame y PCGame modules commented

--- Mobiles/AndroidGame/src/main/java/com/example/assignement1/MainActivity.java
+++ Mobiles/AndroidGame/src/main/java/com/example/assignement1/MainActivity.java
@@ -7,7 +7,10 @@ import com.example.logic.States.LoadingState;
 
 public class MainActivity extends Game {
 
-
+    /**
+     *
+     * @return estado de juego que se va a cargar nada más empiece la aplicación
+     */
     public State getStartState() {
         return new LoadingState(this);
     }
@@ -18,5 +21,5 @@ public class MainActivity extends Game {
 //hebras de java swing y main
 
 //android gestiona nuestro ciclo de vida (al girar el movil se vuelve a llamar al on create, cierra la actividad
-//las hebras creadas por el programador no se cierran las hebras y siguen en funcionamiento
+//las hebras creadas por el programador no se cierran y siguen en funcionamiento
 //hebra nueva se debería crear en un nuevo fichero
--- Mobiles/PCGame/src/main/java/com/example/pcgame/PCMain.java
+++ Mobiles/PCGame/src/main/java/com/example/pcgame/PCMain.java
@@ -6,14 +6,33 @@ import com.example.pcengine.Game;
 
 public class PCMain extends Game {
 
+    /**
+     * CLASE PC_MAIN
+     * Encargada de lanzar al aplicación para escritorio
+     */
+
+    /**
+     * Contructora de la clase
+     * @param windowName Nombre de la ventana
+     * @param w Anchura de la ventana
+     * @param h Altura de la ventana
+     */
     private PCMain(String windowName, int w, int h){
         super(windowName, w, h );
     }
 
+    /**
+     *
+     * @return estado de juego que se va a cargar nada más empiece la aplicación
+     */
     public State getStartState() {
         return new LoadingState(this);
     }
 
+    /**
+     * Crea una instanca de PCMAIN
+     * @param args parámetro necesario del main de PC
+     */
     public static void main(String[] args){
         PCMain game = new PCMain("Switch Dash", 540, 960);
     }
